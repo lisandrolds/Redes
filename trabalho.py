dd076ece5ac061cb1089c61c145c73bb74a6ee85
André Basso de Almeida <andrebasso92@gmail.com>
2014-06-01 23:29:43 -0300

    Update trabalho.py
    Com a busca criada, conseguimos fazer a conexao por socket e salvar o html da pagina

--- trabalho.py
+++ trabalho.py
@@ -1,6 +1,50 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+import socket
+import re
+import sys
+
+
+def divide(url):
+	diretorio = url.split("/", 1)[-1]
+
+	if diretorio == url:
+		diretorio = "/"
+	else:
+		diretorio = "/" + diretorio 
+
+	return diretorio
+
+
+def search(url, profundidade):        
+	
+	url = url.split("http://")[-1]
+	
+	porta = 80
+	host = url.split("/")[0]
+	diretorio = divide(url)
+	conection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    
+	try:
+		conection.connect((host, porta))
+		conection.send("GET %s HTTP/1.1\r\nHost: %s\r\n\r\n" %(diretorio, host))
+	except:
+		print "Erro"
+		return
+		
+	html = ''
+	stream = ''
+	while(len(stream) != 0):
+		try:
+			stream = conection.recv(1024)
+			html = html + stream
+		except:
+			break
+	conection.close()
+	
+	print html
+
 
 def main(argc, argv):
     
