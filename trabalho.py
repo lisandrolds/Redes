eed08f1bf8581c288003bbc0609b48916c7d9385
lisandrolds <lldsilva@inf.ufpel.edu.br>
2014-06-01 23:38:50 -0300

    Atualizando

--- trabalho.py
+++ trabalho.py
@@ -5,7 +5,6 @@ import socket
 import re
 import sys
 
-
 def divide(url):
 	diretorio = url.split("/", 1)[-1]
 
@@ -15,7 +14,6 @@ def divide(url):
 		diretorio = "/" + diretorio 
 
 	return diretorio
-
 
 def search(url, profundidade):        
 	
@@ -30,22 +28,46 @@ def search(url, profundidade):
 		conection.connect((host, porta))
 		conection.send("GET %s HTTP/1.1\r\nHost: %s\r\n\r\n" %(diretorio, host))
 	except:
-		print "Erro"
+		print "Impossible connect TCP or send HTTP request.."
 		return
 		
 	html = ''
-	stream = ''
+	stream = "Initial Value"
 	while(len(stream) != 0):
 		try:
 			stream = conection.recv(1024)
 			html = html + stream
 		except:
+			print "Connection closed! "
 			break
 	conection.close()
+		
+	lista = re.findall(r'a href=[\'"]?([^\'" >]+)', html)
+	lista = retirandoURLsRepitidas(lista)
 	
-	print html
+	
+	for i in range(len(lista)):
+		print lista[i] + ', profundidade: ' + str(profundidade)
+		print 'Imagens dessa pagina: \n'
+		
+		img = re.findall(r'img src=[\'"]?([^\'" >]+)', html)
+		img = retirandoURLsRepitidas(img)
+	
+		for imagem in img:
+			print imagem
+		
+		if(profundidade - 1 > 0):
+			search(lista[i], profundidade - 1)
+	
+	return	
 
-
+def retirandoURLsRepitidas(lista):
+	novaLista = []
+	for url in lista :
+		if not(url in novaLista) :
+			novaLista.append(url)
+	return novaLista	
+		
 def main(argc, argv):
     
 	profundidade = int(argv[1])
@@ -54,5 +76,6 @@ def main(argc, argv):
 	print 'Url: ' + url
 	print 'Profundidade: ' + str(profundidade)
 	
+	search(url, profundidade)
 	
 main(len(sys.argv), sys.argv)
