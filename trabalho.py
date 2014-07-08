f07b8fe11807b70a878c597f336919822c555d75
lisandrolds <lldsilva@inf.ufpel.edu.br>
2014-07-07 23:08:12 -0300

    Colocando SSL mas está com alguns problemas

--- trabalho.py
+++ trabalho.py
@@ -4,6 +4,7 @@
 import socket
 import re
 import sys
+import ssl
 
 def divide(url):
 	diretorio = url.split("/", 1)[-1]
@@ -19,17 +20,30 @@ def search(url, profundidade):
 	
 	url = url.split("http://")[-1]
 	
-	porta = 80
+	port = 80
 	host = url.split("/")[0]
 	diretorio = divide(url)
 	conection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     
+	## adicionando ssl
+	port = 443
+	
+	sslSocket = ssl.wrap_socket(conection, ca_certs = "ca-certificates.crt", cert_reqs = ssl.CERT_REQUIRED)
+	
 	try:
-		conection.connect((host, porta))
-		conection.send("GET %s HTTP/1.1\r\nHost: %s\r\n\r\n" %(diretorio, host))
+		sslSocket.connect((host, port))
 	except:
-		print "Impossible connect TCP or send HTTP request.."
-		return
+		certificadoInvalido = True
+	
+	validacao = ''
+	if certificadoInvalido :
+		try:
+			sslSocket = ssl.wrap_socket(socket.socket(socket.AF_INET, socket.SOCK_STREAM),
+								ca_certs = "ca-certificates.crt")
+			sslSocket.connect((host, port))
+			validacao = 'Certificado nao confiavel'
+		except:
+			print "Erro de certificacao\n"
 		
 	html = ''
 	stream = "Initial Value"
