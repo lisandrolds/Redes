cf508b71fcf6ac199dfb8a71acf5340839e3cf20
André Basso de Almeida <andrebasso92@gmail.com>
2014-07-10 13:54:32 -0300

    Finalmente funcionando

--- trabalho.py
+++ trabalho.py
@@ -5,6 +5,7 @@ import socket
 import re
 import sys
 import ssl
+import pprint
 
 def divide(url):
 	diretorio = url.split("/", 1)[-1]
@@ -20,41 +21,48 @@ def search(url, profundidade):
 	
 	url = url.split("http://")[-1]
 	
-	port = 80
 	host = url.split("/")[0]
 	diretorio = divide(url)
 	conection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     
 	## adicionando ssl
 	port = 443
-	
-	sslSocket = ssl.wrap_socket(conection, ca_certs = "ca-certificates.crt", cert_reqs = ssl.CERT_REQUIRED)
+	assinado = True
+	sslSocket = ssl.wrap_socket(conection, ca_certs = "/etc/ssl/certs/ca-certificates.crt", cert_reqs = ssl.CERT_REQUIRED)
 	
 	try:
 		sslSocket.connect((host, port))
 	except:
-		certificadoInvalido = True
-	
-	validacao = ''
-	if certificadoInvalido :
 		try:
-			sslSocket = ssl.wrap_socket(socket.socket(socket.AF_INET, socket.SOCK_STREAM),
-								ca_certs = "ca-certificates.crt")
+			conection.close();
+			conection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			sslSocket = ssl.wrap_socket(conection, cert_reqs=ssl.CERT_NONE)
 			sslSocket.connect((host, port))
-			validacao = 'Certificado nao confiavel'
+			assinado = False
 		except:
-			print "Erro de certificacao\n"
-		
+			print "SSL com erro\n"
+			return
+	
+	if not(assinado):
+		print "Certificado SSL!\n"
+	
+	print pprint.pformat(sslSocket.getpeercert())
+
+	try:
+		sslSocket.send("GET %s HTTP/1.1\r\nHost: %s\r\n\r\n" %(diretorio, host))
+	except:
+		print "HTTP request indisponivel!\n" 
+	
 	html = ''
 	stream = "Initial Value"
 	while(len(stream) != 0):
 		try:
-			stream = conection.recv(1024)
+			stream = sslSocket.recv(1024)
 			html = html + stream
 		except:
 			print "Connection closed! "
 			break
-	conection.close()
+	sslSocket.close()
 		
 	lista = re.findall(r'a href=[\'"]?([^\'" >]+)', html)
 	lista = retirandoURLsRepitidas(lista)
@@ -73,8 +81,10 @@ def search(url, profundidade):
 		print '\n'
 		
 		if(profundidade - 1 > 0):
-			search(lista[i], profundidade - 1)
-	
+			return search(lista[i], profundidade - 1)
+		else:
+			return
+		
 	return	
 
 def retirandoURLsRepitidas(lista):
